31210cabeb87759c171b907361a30882048c241a
daiki-fujii <d0a5i16ki@gmail.com>
2021-03-12 01:49:18 +0900

    viewに書かれていたロジックをcontrollerに移動

--- app/controllers/judge_controller.rb
+++ app/controllers/judge_controller.rb
@@ -2,14 +2,18 @@ class JudgeController < ApplicationController
 
   def index
     @hands = params[:hands]
+    @messages = []
     if @hands
       judge = Judge.new(@hands)
       res = judge.main
       if res[:type] == "error"
-        @msg = res[:msg]
-        @contents = res[:contents]
+        @messages.push(res[:msg])
+        contents = res[:contents]
+        contents.each do |key, value|
+          @messages.push("#{key}番目のカード指定文字が不正です。(#{value})")
+        end
       else
-        @res = res[:msg]
+        @messages.push(res[:msg])
       end
     end
   end
