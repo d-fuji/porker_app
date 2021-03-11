c6e769cd3753b6c4ec3be65cfd73c8b035987f3c
daiki-fujii <d0a5i16ki@gmail.com>
2021-03-11 09:37:23 +0900

    インデント修正

--- app/controllers/judge_controller.rb
+++ app/controllers/judge_controller.rb
@@ -2,15 +2,15 @@ class JudgeController < ApplicationController
 
   def index
     @hands = params[:hands]
-        if @hands
-            judge = Judge.new(@hands)
-            res = judge.main
-            if res[:type] == "error"
-              @msg = res[:msg]
-              @contents = res[:contents]
-            elsif res[:type] == "result"
-              @res = res[:msg]
-            end
-        end
+    if @hands
+      judge = Judge.new(@hands)
+      res = judge.main
+      if res[:type] == "error"
+        @msg = res[:msg]
+        @contents = res[:contents]
+      elsif res[:type] == "result"
+        @res = res[:msg]
+      end
+    end
   end
 end
