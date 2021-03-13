42bd22b48e1aca5e7ca4a1bcd20e3466ea0c8f35
daiki-fujii <d0a5i16ki@gmail.com>
2021-03-13 21:52:19 +0900

    controllerの無駄な部分を削除&modelに一部移動

--- app/controllers/judge_controller.rb
+++ app/controllers/judge_controller.rb
@@ -3,20 +3,9 @@ class JudgeController < ApplicationController
   def index
     @hands = params[:hands]
     @messages = []
-    # POSTリクエストの時にジャッジの判別ロジックを動作させないため
-    @hands = params[:hands]
     if @hands
       judge = Judge.new(@hands)
-      res = judge.main
-      if res[:type] == "error"
-        @messages.push(res[:msg])
-        contents = res[:contents]
-        contents.each do |key, value|
-          @messages.push("#{key}番目のカード指定文字が不正です。(#{value})")
-        end
-      else
-        @messages.push(res[:msg])
-      end
+      @messages = judge.return_messages
     end
   end
 end
--- app/models/judge.rb
+++ app/models/judge.rb
@@ -1,57 +1,18 @@
 class Judge
 
+    attr_accessor :response
     attr_accessor :hands
     attr_accessor :cards
     attr_accessor :suits, :numbers
     attr_accessor :counter_two, :counter_three, :counter_four
 
     def initialize(hands)
+        self.response = []
         self.hands = hands
         self.cards = []
         hands = hands.split
         hands.each do |hand|
-            self.cards.push Card.new(suit: hand[0], number: hand[1..])
-        end
-    end
-
-    def main
-        validation = Validation.new
-        if validation.is_invalid_hands_format?(hands: self.hands)
-            return INVALID_HANDS_FORMAT_ERROR
-        elsif validation.is_invalid_card?(cards: self.cards)
-            # 定数として管理したいが、contentsが変数のためどうしよう...
-            contents = validation.show_invalid_cards(cards: self.cards)
-            invalid_card_error = INVALID_CARD_ERROR
-            invalid_card_error[:contents] = contents
-            return invalid_card_error
-        elsif validation.has_same_card?(cards: self.cards)
-            return HAS_SAME_CARD_ERROR
-        else
-            judge
-        end      
-    end
-
-    def judge
-        preprocess
-
-        if is_flash? && is_straight?
-            return STRAIGHT_FLASH
-        elsif is_four_of_a_kind?
-            return FOUR_OF_A_KIND
-        elsif is_full_house?
-            return FULL_HOUSE
-        elsif is_flash?
-            return FLASH
-        elsif is_straight?
-            return STRAIGHT
-        elsif is_three_of_a_kind?
-            return THREE_OF_A_KIND
-        elsif is_twopair?
-            return TWO_PAIR
-        elsif is_onepair?
-            return ONE_PAIR
-        else
-            return HIGH_CARD
+            self.cards.push(Card.new(suit: hand[0], number: hand[1..]))
         end
     end
 
@@ -61,12 +22,10 @@ class Judge
         self.counter_four = 0
         self.numbers = {"1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 0, "7" => 0, "8" => 0, "9" => 0, "10" => 0, "11" => 0, "12" => 0, "13" => 0}
         self.suits = {"S" => 0, "H" => 0, "D" => 0, "C" => 0}
-
         self.cards.each do |card|
             self.numbers[card.number] += 1
             self.suits[card.suit] += 1
         end
-
         self.numbers.each do |key, value|
             if value == 2
                 self.counter_two += 1
@@ -75,6 +34,57 @@ class Judge
             elsif value == 4
                 self.counter_four += 1
             end
+        end
+    end
+
+    def return_messages
+        if satisfy_validation_item?
+            judge_poker_role
+        end
+        return self.response
+    end
+
+    def satisfy_validation_item?
+        validation = Validation.new
+        if validation.is_invalid_hands_format?(hands: self.hands)
+            self.response.push(INVALID_HANDS_FORMAT_ERROR)
+            return false
+        elsif validation.is_invalid_card?(cards: self.cards)
+            self.response.push(INVALID_CARD_ERROR)
+            invalid_cards = validation.show_invalid_cards(cards: self.cards)
+            invalid_cards.each do |key, value|
+                self.response.push("#{key}番目のカード指定文字が不正です。(#{value})")
+            end
+            return false
+        elsif validation.has_same_card?(cards: self.cards)
+            self.response.push(HAS_SAME_CARD_ERROR)
+            return false
+        else
+            return true
+        end
+    end
+
+    def judge_poker_role
+        preprocess
+        
+        if is_flash? && is_straight?
+            self.response.push(STRAIGHT_FLASH)
+        elsif is_four_of_a_kind?
+            self.response.push(FOUR_OF_A_KIND)
+        elsif is_full_house?
+            self.response.push(FULL_HOUSE)
+        elsif is_flash?
+            self.response.push(FLASH)
+        elsif is_straight?
+            self.response.push(STRAIGHT)
+        elsif is_three_of_a_kind?
+            self.response.push(THREE_OF_A_KIND)
+        elsif is_twopair?
+            self.response.push(TWO_PAIR)
+        elsif is_onepair?
+            self.response.push(ONE_PAIR)
+        else
+            self.response.push(HIGH_CARD)
         end
     end
     
